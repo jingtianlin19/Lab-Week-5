fadaf963a3c2c1bf3412dcb50cf5aca512aa75d9
jingtianlin <linjingtian@easyretailpro.com>
2021-10-25 08:31:21 +0800

    update

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,16 +3,25 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  const selectHorn = document.getElementsByName('horn');
+  var selectHorn = document.getElementsById('horn-select');
   var image = document.querySelector('section img');
   selectHorn.addEventListener('select', imgSelector(image))
+  var selectSound = document.getElementById('volume');
+  var soundIcon = document.querySelector('div img');
+  // selectSound.addEventListener('input', soundSelector(this.value));
 }
 function imgSelector(image) {
-    console.log(this.value);
-    switch(this.value) {
+    switch(image) {
       default : return;
       case 'air-horn' : image = '../images/air-horn.svg';
       case 'car-horn' : image = '../images/car-horn.svg';
       case 'party-horn' : image = '../imagesparty-horn.svg';  
     }
+    return;
 }
+// function soundSelector(vol) {
+//   switch(vol) {
+//     default : return;
+//     case 1 < vol < 33 : 
+//   }
+// }
