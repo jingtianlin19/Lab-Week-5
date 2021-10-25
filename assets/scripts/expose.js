3c5945cb4002a2f78db0d1350e5638ff164720e4
jingtianlin <linjingtian@easyretailpro.com>
2021-10-25 10:04:46 +0800

    update

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -33,7 +33,7 @@ function soundSelector(e) {
   var soundIcon = document.querySelector('div img');
   var audio = document.querySelector('audio')
   const vol = e.target.value;
-  audio.volume = vol;
+  audio.setAttribute('volume', vol/100);
   if (vol == 0) {
     soundIcon.setAttribute('src', 'assets/icons/volume-level-0.svg');
     soundIcon.setAttribute('alt', 'Volume level 0');
@@ -52,6 +52,6 @@ function soundSelector(e) {
   }
 }
 function playsound() {
-  const sound = document.querySelector('audio');
+  // const sound = document.querySelector('audio');
   sound.play();
 }
