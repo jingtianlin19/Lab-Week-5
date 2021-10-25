7a41db8ca815ae8f947a4acb8b59c6966ed832f9
jingtianlin <linjingtian@easyretailpro.com>
2021-10-25 08:21:26 +0800

    update

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,14 +4,15 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const selectHorn = document.getElementsByName('horn');
-  const image = document.querySelector('img');
+  var image = document.querySelector('section img');
   selectHorn.addEventListener('selectHorn', () => {
     console.log(this.value);
     switch(this.value) {
       default : return;
       case 'air-horn' : image = '../images/air-horn.svg';
       case 'car-horn' : image = '../images/car-horn.svg';
-      case 'party-horn' : image = '../imagesparty-horn.svg';
+      case 'party-horn' : image = '../imagesparty-horn.svg';  
     }
+    return;
   })
 }
