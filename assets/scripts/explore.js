bdc8663f2d610f64d62cc2e6bd6597ca89f626ac
jingtianlin <linjingtian@easyretailpro.com>
2021-10-25 13:30:10 +0800

    update

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,14 +8,15 @@ function init() {
   select.addEventListener('change', selectSound);
   const textIn = document.getElementById('text-to-speak');
   textIn.addEventListener('change', (e)=>{textToRead = e.target.value});
-
+  const read = document.querySelector('button');
+  read.addEventListener('click', readTxt);
 }
 function selectSound() {
   const options = document.getElementById('voice-select')
   const voices = synth.getVoices();
   for (voice in voices) {
     var option = document.createElement('option');
-    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    option.textContent = voices[i].name + ' (' + voice.lang + ')';
     if(voice.default) {
       option.textContent += ' -- DEFAULT';
     }
@@ -23,5 +24,15 @@ function selectSound() {
     option.setAttribute('data-lang', voice.lang);
     option.setAttribute('data-name', voice.name);
     options.appendChild(option);
+  }
+}
+
+function readTxt() {
+  const icon = document.querySelector('img');
+  var speaking = synth.speaking;
+  icon.setAttribute('src', 'assets/images/smiling-open.png');
+  synth.speak(textToRead);
+  if (speaking == 0) {
+    icon.setAttribute('src', 'assets/images/smiling.png');
   }
 }
